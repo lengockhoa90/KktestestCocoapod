21a93f1f3a3c7adcd8fdb4a99bd2e644a7c5c99b
khoa <lengockhoa90@gmail.com>
2014-11-01 14:40:43 +0700

    submit cocoa pos test

---
+++ Example/Pods/Target Support Files/Pods-KktestestCocoapod-KktestestCocoapod/Pods-KktestestCocoapod-KktestestCocoapod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KktestestCocoapod_KktestestCocoapod : NSObject
+@end
+@implementation PodsDummy_Pods_KktestestCocoapod_KktestestCocoapod
+@end
---
+++ Example/Pods/Target Support Files/Pods-KktestestCocoapod/Pods-KktestestCocoapod-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KktestestCocoapod
+#define COCOAPODS_POD_AVAILABLE_KktestestCocoapod
+#define COCOAPODS_VERSION_MAJOR_KktestestCocoapod 0
+#define COCOAPODS_VERSION_MINOR_KktestestCocoapod 1
+#define COCOAPODS_VERSION_PATCH_KktestestCocoapod 0
+
